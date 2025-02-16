6bd25d18ec7b3ba4a1c5a76c164dd5c27836241b
flatypus [mac] <yhc3141@gmail.com>
2025-02-16 02:48:35 -0800

    ok

--- embedded/embedded.ino
+++ embedded/embedded.ino
@@ -2,8 +2,10 @@
 #include <Arduino.h>
 
 Servo servoFWD;
+Servo servoFWD_LEFT;
 Servo servoFWD_RIGHT;
 Servo servoLEFT;
+Servo servoRIGHT;
 
 void pressure(int dir)
 {
@@ -15,12 +17,20 @@ void pressure(int dir)
     s->attach(6);
     break;
   case 1:
-    s = &servoFWD_RIGHT;
+    s = &servoFWD_LEFT;
     s->attach(9);
     break;
   case 2:
+    s = &servoFWD_RIGHT;
+    s->attach(5);
+    break;
+  case 3:
     s = &servoLEFT;
     s->attach(10);
+    break;
+  case 4:
+    s = &servoRIGHT;
+    s->attach(11);
     break;
   default:
     return;
@@ -33,6 +43,11 @@ void pressure(int dir)
 void setup()
 {
   Serial.begin(9600);
+  pressure(0);
+  pressure(1);
+  pressure(2);
+  pressure(3);
+  pressure(4);
 }
 
 void loop()
